64947572705350f20f32d9f9329ae632761089a6
tohir <st.coder2005@gmail.com>
2024-08-24 08:21:44 +0500

    notification

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:calendar_app/core/app.dart';
 import 'package:calendar_app/core/dependency_injection.dart';
 import 'package:flutter/material.dart';
 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await eventInit();
   runApp(const MyApp());
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_local_notifications
+import flutter_timezone
 import geolocator_apple
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
